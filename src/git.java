1d3040a7641c52cd354144041545ce78458f8d3a
Pali444 <orlofff2018@mail.ru>
2021-11-28 18:46:00 +0300

    second commit

---
+++ src/git.java
@@ -0,0 +1,16 @@
+public class git {
+    public static void main(String[] args) {
+
+
+    int a = 1;
+    int b = 2;
+
+    int result = a + b;
+        int a = 1;
+        int b = 2;
+
+        int result = a + b;
+        
+        System.out.println(result);
+}
+}
